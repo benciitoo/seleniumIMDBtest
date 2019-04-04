88e19407789deb5577981209e2c9a71c86acabd8
benciitoo <bence.banszegi@gmail.com>
2019-04-04 14:01:51 +0200

    feat(sign in): sign in to IMDB with email and password

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,12 @@
+import keywords.User;
+
 public class App {
 
+    public static void main(String[] args) {
+        User.getMainPage();
+        User.loginToIMDBWithUserEmailAndPassword("bence.banszegi@gmail.com", "imdbpassword");
 
+    }
 
 
 }
--- src/main/java/keywords/User.java
+++ src/main/java/keywords/User.java
@@ -1,8 +1,40 @@
 package keywords;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class User {
 
-    
+    static WebDriver driver = new FirefoxDriver();
+
+    public static void getMainPage() {
+        driver.get("https://imdb.com");
+    }
+
+    public static void navigateToLoginForm() {
+        driver.findElement(By.id("imdb-signin-link")).click();
+        driver.findElement(By.xpath("//*[@id=\"signin-options\"]/div/div[1]/a[1]")).click();
+    }
+
+    public static void sendUserEmailToLoginForm(String userEmail) {
+        driver.findElement(By.id("ap_email")).sendKeys(userEmail);
+    }
+
+    public static void sendUserPasswordToLoginForm(String password){
+        driver.findElement(By.id("ap_password")).sendKeys(password);
+    }
+
+    public static void clickSignInButton(){
+        driver.findElement(By.id("signInSubmit")).click();
+    }
+
+    public static void loginToIMDBWithUserEmailAndPassword (String userEmail, String password){
+        navigateToLoginForm();
+        sendUserEmailToLoginForm(userEmail);
+        sendUserPasswordToLoginForm(password);
+        clickSignInButton();
+    }
 
 
 }
